785fb4508c94e96413eb4eeb67cbf11ab6463f58
attikatt <astrid.e.gustafsson@gmail.com>
2017-12-19 12:46:28 +0100

    Vit text

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,3 +1,5 @@
+'use strict';
+
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
@@ -11,7 +13,8 @@ function drawChart() {
     title: 'Distribution of orders',
     pieHole: 0.4,
     'backgroundColor':'transparent',
-
+    'titleTextStyle': {color:'white', font: 'champagne__limousinesregular', bold:'false'},
+    legend: {textStyle: {color: 'white'}}
   };
 
   var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
