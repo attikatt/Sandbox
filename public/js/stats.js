a03a530bd18a556f0786b5d7586b64667096dbf5
attikatt <astrid.e.gustafsson@gmail.com>
2017-12-20 11:42:14 +0100

    Genererar data

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,13 +1,56 @@
 'use strict';
 
+function updateClock(){
+var now = new Date(),
+    hours = now.getHours(),
+    minutes = now.getMinutes(),
+    seconds = now.getSeconds();
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    };
+document.getElementById('clock').innerHTML = [hours,minutes,seconds].join(':');
+setTimeout(updateClock,1000);
+}
+ updateClock();
+
+ var dataVm = new Vue({
+   el: '#VueDiv',
+   mixins: [sharedVueStuff], // include stuff that is used both in the ordering system and in the kitchen
+   data: {
+     amountSmoothies: 0,
+     amountJuices: 0
+   },
+   methods: {
+     createOrderData: function() {
+      // console.log(this.orders[1].type);
+       //console.log(this.orders)
+     for (var i = 1; i < 4; i += 1) {
+       console.log(i + " " + this.orders[i].type);
+       if (this.orders[i].type === 'juice'){
+         this.amountJuices += 1;
+       }
+       else if (this.orders[i].type === 'smoothie'){
+         this.amountSmoothies += 1;
+       }
+     }
+     return ([
+       ['Type of drink', 'Amount ordered'],
+       ['Smoothie',     this.amountSmoothies],
+       ['Juice',      this.amountJuices]
+     ])
+   }
+ }
+});
+
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
+  var data = google.visualization.arrayToDataTable(dataVm.createOrderData()/*[
     ['Type of drink', 'Amount ordered'],
     ['Smoothie',     11],
     ['Juice',      2]
-  ]);
+  ]*/);
+  //dataVm.createData();
 
   var options = {
     title: 'Distribution of orders',
@@ -21,17 +64,3 @@ function drawChart() {
   var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
   chart.draw(data, options);
 }
-console.log(sharedVueStuff.Data().getAllOrders());
-
-function updateClock(){
-var now = new Date(),
-    hours = now.getHours(),
-    minutes = now.getMinutes(),
-    seconds = now.getSeconds();
-    if (minutes < 10) {
-        minutes = "0" + minutes
-    };
-document.getElementById('clock').innerHTML = [hours,minutes,seconds].join(':');
-setTimeout(updateClock,1000);
-}
- updateClock();
