67d375c25b49403575c46c15e63bc5b65f0ac5b0
attikatt <astrid.e.gustafsson@gmail.com>
2018-01-04 18:33:27 +0100

    hittade bugg

--- public/js/lager.js
+++ public/js/lager.js
@@ -85,13 +85,14 @@ function clearSaldoField(){
 function scrollFunction(value){
   //divideIngredientsIntoCategories()
   var numericValue = (parseInt(value, 36)-9);
+  console.log(parseInt('Ä',36)-9);
   for (var i = 0; i < vm.ingredients.length; i++){
     if ((parseInt(vm.ingredients[i].ingredient_sv[0].toUpperCase(), 36)-9)===numericValue){
       var element = document.getElementById(vm.ingredients[i].ingredient_id);
       element.scrollIntoView();
       break;
     }
-    else if ((parseInt(vm.ingredients[i].ingredient_sv[0].toUpperCase(), 36)-9)>numericValue){
+    else if ((parseInt(vm.ingredients[i].ingredient_sv[0].toUpperCase(), 36)-9) > numericValue){
       var element = document.getElementById(vm.ingredients[i-1].ingredient_id);
       element.scrollIntoView();
       break;
